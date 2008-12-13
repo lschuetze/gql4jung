fe9a68bfcc2cbf54fc970f5e735fd85ecff1f51a
jens.dietrich <jens.dietrich@gmail.com>
2008-12-13 08:21:10 +0000

    some remarks and sketches by jens

---
+++ gpl4jung/src/nz/ac/massey/cs/gpl4jung/impl/GQLImpl.java
@@ -0,0 +1,24 @@
+package nz.ac.massey.cs.gpl4jung.impl;
+
+import edu.uci.ics.jung.graph.Graph;
+import nz.ac.massey.cs.gpl4jung.GQL;
+import nz.ac.massey.cs.gpl4jung.Motif;
+import nz.ac.massey.cs.gpl4jung.QueryOptimizer;
+import nz.ac.massey.cs.gpl4jung.ResultListener;
+
+public class GQLImpl implements GQL {
+
+	@Override
+	public void query(Graph graph, Motif motif, ResultListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void query(Graph graph, Motif motif, ResultListener listener,
+			QueryOptimizer optimizer) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
