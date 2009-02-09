d885faa09ef431b05e3be143964a11c2616f219f
alishah.iiui <alishah.iiui@df725a96-ab7d-11dd-b9e7-8d5594d729c2>
2009-02-09 21:37:05 +0000

    modified ConstraintSchedulerImpl, for resolving constraints

--- gpl4jung/src/nz/ac/massey/cs/gpl4jung/impl/ConstraintSchedulerImpl.java
+++ gpl4jung/src/nz/ac/massey/cs/gpl4jung/impl/ConstraintSchedulerImpl.java
@@ -5,12 +5,17 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.commons.collections.IteratorUtils;
+
+import edu.uci.ics.jung.graph.Edge;
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.Vertex;
 
+import nz.ac.massey.cs.gpl4jung.ConnectedVertex;
 import nz.ac.massey.cs.gpl4jung.Constraint;
 import nz.ac.massey.cs.gpl4jung.Motif;
 import nz.ac.massey.cs.gpl4jung.PropertyConstraint;
+import nz.ac.massey.cs.gpl4jung.constraints.EdgeConstraint;
 import nz.ac.massey.cs.gpl4jung.constraints.PathConstraint;
 import nz.ac.massey.cs.gpl4jung.constraints.PropertyTerm;
 import nz.ac.massey.cs.gpl4jung.constraints.SimplePropertyConstraint;
@@ -39,6 +44,11 @@ public class ConstraintSchedulerImpl implements ConstraintScheduler {
 			}
 		}
 		for(Constraint c:constraints){
+			if(c instanceof EdgeConstraint){
+				interimConstraints.add(c);
+			}
+		}
+		for(Constraint c:constraints){
 			if(c instanceof PathConstraint){
 				interimConstraints.add(c);
 			}
@@ -49,10 +59,37 @@ public class ConstraintSchedulerImpl implements ConstraintScheduler {
 
 	@Override
 	public Constraint selectNext(Graph g, List<Constraint> agenda, Bindings bindings) {
-		// TODO Auto-generated method stub
+		List<Constraint> constraints = new ArrayList<Constraint>();
+		for(Iterator itr=agenda.iterator();itr.hasNext();){
+			Object c = (Object) itr.next();
+			if(c instanceof PropertyConstraint){
+				SimplePropertyConstraint<Vertex> vc = (SimplePropertyConstraint<Vertex>) c;
+				Term[] terms = vc.getTerms();
+				PropertyTerm key = (PropertyTerm) terms[0];
+				ValueTerm value = (ValueTerm) terms[1];
+				Object instance = bindings.lookup(key.getKey());
+				if (instance == null) {
+	                // not yet binded
+					Bindings nextBinding = createBindingMap(bindings);
+                    // add new binding
+					nextBinding.bind(key.getKey(), value.getValue());
+                    selectNext(g, agenda,nextBinding);
+                    releaseBindingMap(nextBinding);
+				}
+				else {
+	               
+	                	selectNext(g, agenda,bindings);
+				}
+			}
+			else if (c instanceof EdgeConstraint){
+				Vertex v = getVertexFromGraph(g, ((EdgeConstraint) c).getSource());
+				Iterator<ConnectedVertex<Edge>> pt =  ((EdgeConstraint) c).getPossibleTargets(g, v);
+				List<ConnectedVertex<Edge>> list = IteratorUtils.toList(pt); //list of all possible targets
+				//TODO: Setting up bindings
+			}
+		}
 		return null;
 	}
-	
 	public List<Constraint> sort(List<Constraint> unsortedPropConstraints){
 		
 		java.util.Comparator comp =  new java.util.Comparator() {
@@ -82,5 +119,45 @@ public class ConstraintSchedulerImpl implements ConstraintScheduler {
 		List<Constraint> sortedConstraints = unsortedPropConstraints;
 		return sortedConstraints;
 		
+	}
+	
+	/**
+     * Create the next map containing bindings.
+     * @param map a map containing binding
+     * @return a map
+     */
+    private Bindings createBindingMap(Bindings bindings) {
+    	bindings.gotoChildLevel();
+    	return bindings;
+    }
+
+    /**
+     * Release the map containing bindings. 
+     * @param map a map containing bindings.
+     */
+    private void releaseBindingMap(Bindings bindings) {
+    	bindings.gotoParentLevel();
+    }
+
+    /**
+     * Check whether a binding map contains an object. 
+     * @param map a binding map
+     * @param obj an object
+     * @return a boolean
+     */
+    private boolean containsValue(Bindings map, Object value) {
+        return map.containsValue(value);
+    }
+    
+    private Vertex getVertexFromGraph(Graph g, String vertexname){
+		String key="name";
+		Vertex vert = null;
+		for (Iterator iter = g.getVertices().iterator(); iter.hasNext();){
+			Vertex v = (Vertex) iter.next();
+			if(v.getUserDatum(key).equals(vertexname)){
+				vert = (Vertex) v;
+			}
+		}
+		return vert;
 	}
 }
