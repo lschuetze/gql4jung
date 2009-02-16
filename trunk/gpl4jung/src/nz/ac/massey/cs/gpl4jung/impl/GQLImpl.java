9d835ba0973f57035a2a4fdb87865438534b9740
alishah.iiui <alishah.iiui@df725a96-ab7d-11dd-b9e7-8d5594d729c2>
2009-02-16 22:52:22 +0000

    modified solveConstraint() in GQLImpl class

--- trunk/gpl4jung/src/nz/ac/massey/cs/gpl4jung/impl/GQLImpl.java
+++ trunk/gpl4jung/src/nz/ac/massey/cs/gpl4jung/impl/GQLImpl.java
@@ -3,17 +3,33 @@ package nz.ac.massey.cs.gpl4jung.impl;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.commons.collections.IteratorUtils;
+
+import edu.uci.ics.jung.graph.Edge;
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.Vertex;
+import nz.ac.massey.cs.gpl4jung.ConnectedVertex;
 import nz.ac.massey.cs.gpl4jung.Constraint;
 import nz.ac.massey.cs.gpl4jung.GQL;
 import nz.ac.massey.cs.gpl4jung.Motif;
+import nz.ac.massey.cs.gpl4jung.Path;
+import nz.ac.massey.cs.gpl4jung.PropertyConstraint;
 import nz.ac.massey.cs.gpl4jung.QueryOptimizer;
 import nz.ac.massey.cs.gpl4jung.ResultListener;
 import nz.ac.massey.cs.gpl4jung.constraints.EdgeConstraint;
+import nz.ac.massey.cs.gpl4jung.constraints.PathConstraint;
+import nz.ac.massey.cs.gpl4jung.constraints.PropertyTerm;
+import nz.ac.massey.cs.gpl4jung.constraints.Term;
+import nz.ac.massey.cs.gpl4jung.constraints.ValueTerm;
 
 public class GQLImpl implements GQL {
-
+	
+	private ConstraintScheduler constraintScheduler = new ConstraintSchedulerImpl();
+	private boolean cancelled = false;
+    private int counter = 0;
+    private int instanceCounter = 0;
+    private Graph graph = null;
+	
 	@Override
 	public void query(Graph graph, Motif motif, ResultListener listener) {
 		// TODO Auto-generated method stub
@@ -28,21 +44,71 @@ public class GQLImpl implements GQL {
 	}
 	
 	public void solveConstraints(List<Constraint> constraints, Bindings bindings){
-//        // not yet binded
-//		Bindings nextBinding = createBindingMap(bindings);
-//        // add new binding
-//		nextBinding.bind(key.getKey(), value.getValue());
-//        selectNext(g, agenda,nextBinding);
-//        releaseBindingMap(nextBinding);
-		
-//		else {
-//        
-//         	selectNext(g, agenda,bindings);
-//
-//		else if (c instanceof EdgeConstraint){
-//			Vertex v = getVertexFromGraph(g, ((EdgeConstraint) c).getSource());
-//			Iterator<ConnectedVertex<Edge>> pt =  ((EdgeConstraint) c).getPossibleTargets(g, v);
-//			List<ConnectedVertex<Edge>> list = IteratorUtils.toList(pt); //list of all possible targets
+		counter++;
+		if(cancelled){
+			return;
+		}
+		for(Iterator itr=constraints.iterator();itr.hasNext();){
+			Object c = (Object) itr.next();
+			if (c instanceof PropertyConstraint){
+				PropertyConstraint constraint = (PropertyConstraint) c;
+				Term[] terms = constraint.getTerms();
+				PropertyTerm key = (PropertyTerm) terms[0];
+				ValueTerm value = (ValueTerm) terms[1];
+				Object instance = bindings.lookup(key.getKey());
+				if (instance==null){
+					// not yet binded
+					Bindings nextBinding = createBindingMap(bindings);
+					// add new binding
+					nextBinding.bind(key.getKey(), value.getValue());
+					releaseBindingMap(nextBinding);
+				}
+			}
+			else if (c instanceof EdgeConstraint){
+				EdgeConstraint edgeConstraint = (EdgeConstraint) c;
+				String source = edgeConstraint.getSource();
+				String target = edgeConstraint.getTarget();
+				Vertex sourceVertex = getVertexFromGraph(graph, source);
+				Vertex targetVertex = getVertexFromGraph(graph, target);
+				Object instance1 = bindings.lookup(source);
+				Object instance2 = bindings.lookup(target);
+				if(instance1==null && instance2!=null){
+					Iterator<ConnectedVertex<Edge>> possibleSources = edgeConstraint.getPossibleSources(graph, targetVertex);
+					List<ConnectedVertex<Edge>> sources = IteratorUtils.toList(possibleSources); //list of all possible sources
+				}
+				else if (instance1!=null && instance2==null){
+					Iterator<ConnectedVertex<Edge>> possibleTargets =  edgeConstraint.getPossibleTargets(graph, sourceVertex);
+					List<ConnectedVertex<Edge>> targets = IteratorUtils.toList(possibleTargets); //list of all possible targets
+				}
+				else if (instance1!=null && instance2!=null){
+					if(edgeConstraint.check(graph, sourceVertex, targetVertex)!=null){
+						
+					}
+				}
+			}
+			else if (c instanceof PathConstraint){
+				PathConstraint pathConstraint = (PathConstraint) c;
+				String source = pathConstraint.getSource();
+				String target = pathConstraint.getTarget();
+				Vertex sourceVertex = getVertexFromGraph(graph, source);
+				Vertex targetVertex = getVertexFromGraph(graph, target);
+				Object instance1 = bindings.lookup(source);
+				Object instance2 = bindings.lookup(target);
+				if(instance1==null && instance2!=null){
+					Iterator<ConnectedVertex<Path>> possibleSources = pathConstraint.getPossibleSources(graph, targetVertex);
+					List<ConnectedVertex<Path>> sources = IteratorUtils.toList(possibleSources); //list of all possible sources
+				}
+				else if (instance1!=null && instance2==null){
+					Iterator<ConnectedVertex<Path>> possibleTargets =  pathConstraint.getPossibleTargets(graph, sourceVertex);
+					List<ConnectedVertex<Path>> targets = IteratorUtils.toList(possibleTargets); //list of all possible targets
+				}
+				else if (instance1!=null && instance2!=null){
+					if(pathConstraint.check(graph, sourceVertex, targetVertex)!=null){
+						
+					}
+				}
+			}
+		}	      	
 	}
 
 	/**
