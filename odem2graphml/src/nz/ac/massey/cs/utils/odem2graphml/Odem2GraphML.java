0fccad96788d1033613781953b7d7d9f27438093
alishah.iiui <alishah.iiui@df725a96-ab7d-11dd-b9e7-8d5594d729c2>
2009-03-16 22:45:10 +0000

    modified main algorithm to show dependencies information as nodes and edges, which was previously stored in 'reference' attribute of every node

--- odem2graphml/src/nz/ac/massey/cs/utils/odem2graphml/Odem2GraphML.java
+++ odem2graphml/src/nz/ac/massey/cs/utils/odem2graphml/Odem2GraphML.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -24,7 +27,7 @@ import org.xml.sax.SAXException;
 public class Odem2GraphML {
 	
 
-	private List<String> nodes;
+	private Map<Integer, String> nodes;
 	private List<TempEdge> tempEdges;
 	private List<String> edges;
 
@@ -61,7 +64,7 @@ public class Odem2GraphML {
 		// getAbsolutePath());
 		out.write("\">\n");
 
-		nodes = new ArrayList<String>();
+		nodes = new HashMap<Integer,String>();
 		tempEdges = new ArrayList<TempEdge>();
 		edges = new ArrayList<String>();
 		writeNodes(doc, out);
@@ -75,6 +78,7 @@ public class Odem2GraphML {
 
 	private void writeNodes(Document doc, Writer out) {
 		int nodeId = 0;
+		int nodeId1=0;
 		NodeList containerList = doc.getElementsByTagName("container");
 		for (int i = 0; i < containerList.getLength(); i++) {
 			Node container = containerList.item(i);
@@ -99,108 +103,206 @@ public class Odem2GraphML {
 							String typeStr = typeAttr.getNamedItem("name")
 									.getNodeValue();
 							String typeStr1 = typeAttr.getNamedItem("classification").getNodeValue();
+						
 							try {
-								out.write("<node id=\"");
-								out.write(String.valueOf(nodeId));
-//								out.write("\" class.id=\"");
-//								out.write(String.valueOf(nodeId));
-								out.write("\" container=\"");
-								out.write(containerStr.substring(containerStr
-										.lastIndexOf('/') + 1));
-								out.write("\" namespace=\"");
-								out.write(namespaceStr);
-								out.write("\" name=\"");
-								out.write(typeStr.substring(typeStr
-										.lastIndexOf('.') + 1));
-								out.write("\" cluster=\"null\" isInterface=\"");
-								if(typeAttr.getNamedItem("classification")!=null 
-										&& typeAttr.getNamedItem("classification").getNodeValue()
-										.equals("interface")){
-									out.write("true");
+								if(!nodes.containsValue(typeStr)){
+									out.write("<node id=\"");
+									out.write(String.valueOf(nodeId));
+//										out.write("\" class.id=\"");
+//										out.write(String.valueOf(nodeId));
+									out.write("\" container=\"");
+									out.write(containerStr.substring(containerStr
+											.lastIndexOf('/') + 1));
+									out.write("\" namespace=\"");
+									out.write(namespaceStr);
+									out.write("\" name=\"");
+									out.write(typeStr.substring(typeStr.lastIndexOf('.') + 1));
+									out.write("\" cluster=\"null\" isInterface=\"");
+									if(typeAttr.getNamedItem("classification")!=null 
+											&& typeAttr.getNamedItem("classification").getNodeValue()
+											.equals("interface")){
+										out.write("true");
+									}
+									else
+										out.write("false");
+									out.write("\" type=\"");
+									out.write(typeStr1);
+//										if (typeAttr.getNamedItem("classification") != null)
+//											out.write(String
+//													.valueOf(typeAttr.getNamedItem(
+//															"classification")
+//															.getNodeValue().equals(
+//																	"interface")));
+//										else
+//											out.write("null");
+
+									out.write("\" isAbstract=\"");
+									if (typeAttr.getNamedItem("isAbstract") != null
+											&& typeAttr.getNamedItem("isAbstract")
+													.getNodeValue().equals("yes"))
+										out.write("true");
+									else
+										out.write("false");
+									out.write("\" isException=\"");
+									out.write(String.valueOf(typeStr
+											.endsWith("Exception")));
+									out.write("\" access=\"");
+
+									if (typeAttr.getNamedItem("visibility") != null)
+										out.write(typeAttr.getNamedItem(
+												"visibility").getNodeValue());
+									else
+										out.write("null");
+
+									out.write("\" isBoundry=\"true\" ");
+								
+									//out.write("\" isSelected=\"false\" ");
+//										nodes.add(nodeId, typeStr);
+									if (typeStr.contains(namespaceStr))
+										nodes.put(nodeId, typeStr);
+									else
+										nodes.put(nodeId, namespaceStr + '.'
+												+ typeStr);
+											
+									out.write("/>");
+									out.write('\n');
+									
+									/* to test
+									TempEdge tempEdge = new TempEdge();
+									tempEdge.setSource(String
+											.valueOf(nodeId));
+									tempEdge.setType(typeAttr.getNamedItem("classification")
+											.getNodeValue());
+									tempEdge.setTarget(typeAttr.getNamedItem("name")
+											.getNodeValue()); 
+									tempEdges.add(tempEdge);
+									nodeId++; */
 								}
 								else
-									out.write("false");
-								out.write("\" type=\"");
-								out.write(typeStr1);
-//								if (typeAttr.getNamedItem("classification") != null)
-//									out.write(String
-//											.valueOf(typeAttr.getNamedItem(
-//													"classification")
-//													.getNodeValue().equals(
-//															"interface")));
-//								else
-//									out.write("null");
-
-								out.write("\" isAbstract=\"");
-								if (typeAttr.getNamedItem("isAbstract") != null
-										&& typeAttr.getNamedItem("isAbstract")
-												.getNodeValue().equals("yes"))
-									out.write("true");
-								else
-									out.write("false");
-								out.write("\" isException=\"");
-								out.write(String.valueOf(typeStr
-										.endsWith("Exception")));
-								out.write("\" access=\"");
-
-								if (typeAttr.getNamedItem("visibility") != null)
-									out.write(typeAttr.getNamedItem(
-											"visibility").getNodeValue());
-								else
-									out.write("null");
-
-								out.write("\" isSelected=\"false\" ");
-
-								if (typeStr.contains(namespaceStr))
-									nodes.add(nodeId, typeStr);
-								else
-									nodes.add(nodeId, namespaceStr + '.'
-											+ typeStr);
-
+									nodeId--;
+								
+								//depends-on nodes
 								StringBuffer buffer = new StringBuffer();
 								if (type.getNodeType() == Node.ELEMENT_NODE) {
 									Element typeElement = (Element) type;
 									NodeList relationships = typeElement
 											.getElementsByTagName("depends-on");
-									for (int e = 0; e < relationships
-											.getLength(); e++) {
-										Node relationship = relationships
-												.item(e);
-										NamedNodeMap relationshipAttr = relationship
-												.getAttributes();
+									for (int e = 0; e < relationships.getLength(); e++) {
+										Node relationship = relationships.item(e);
+										NamedNodeMap relationshipAttr = relationship.getAttributes();
+										String relationshipStr = relationshipAttr.getNamedItem("name").getNodeValue();
+										String relationshipStr1 = relationshipAttr.getNamedItem("classification").getNodeValue();
+										
+										if(!nodes.containsValue(relationshipStr)){
+											nodeId1+=nodeId+1;
+											out.write("<node id=\"");
+											out.write(String.valueOf(nodeId1));
+//												out.write("\" class.id=\"");
+//												out.write(String.valueOf(nodeId));
+											out.write("\" container=\"");
+											out.write(containerStr.substring(containerStr
+													.lastIndexOf('/') + 1));
+											out.write("\" namespace=\"");
+											out.write(relationshipStr.substring(0, relationshipStr.lastIndexOf('.')));
+											out.write("\" name=\"");
+											out.write(relationshipStr.substring(relationshipStr
+													.lastIndexOf('.') + 1));
+											out.write("\" cluster=\"null\" isInterface=\"");
+											if(relationshipAttr.getNamedItem("classification")!=null 
+													&& relationshipAttr.getNamedItem("classification").getNodeValue()
+													.equals("interface")){
+												out.write("true");
+											}
+											else
+												out.write("false");
+											out.write("\" type=\"");
+											out.write(typeStr1);//TODO:
+//												if (typeAttr.getNamedItem("classification") != null)
+//													out.write(String
+//															.valueOf(typeAttr.getNamedItem(
+//																	"classification")
+//																	.getNodeValue().equals(
+//																			"interface")));
+//												else
+//													out.write("null");
 
-										TempEdge tempEdge = new TempEdge();
-										tempEdge.setSource(String
-												.valueOf(nodeId));
-										tempEdge.setType(relationshipAttr
-												.getNamedItem("classification")
-												.getNodeValue());
-										tempEdge.setTarget(relationshipAttr
-												.getNamedItem("name")
-												.getNodeValue());
+											out.write("\" isAbstract=\"");
+											if (relationshipAttr.getNamedItem("isAbstract") != null
+													&& relationshipAttr.getNamedItem("isAbstract")
+															.getNodeValue().equals("yes"))
+												out.write("true");
+											else
+												out.write("false");
+											out.write("\" isException=\"");
+											out.write(String.valueOf(relationshipStr
+													.endsWith("Exception")));
+											out.write("\" access=\"");
 
-										buffer.append(relationshipAttr
-												.getNamedItem("name")
-												.getNodeValue());
-										buffer.append('|');
+											if (relationshipAttr.getNamedItem("visibility") != null)
+												out.write(typeAttr.getNamedItem(
+														"visibility").getNodeValue());
+											else
+												out.write("null");
 
-										tempEdges.add(tempEdge);
+											out.write("\" isBoundry=\"false\" ");
+											
+											//out.write("\" isSelected=\"false\" ");
+
+											nodes.put(nodeId1, relationshipStr);
+//												if (relationshipStr.contains(namespaceStr))
+//													nodes.add(nodeId, relationshipStr);
+//												else
+//													nodes.add(nodeId, namespaceStr + '.'
+//															+ relationshipStr);
+													
+											out.write("/>");
+											out.write('\n');
+											//nodeId++;
+											
+											TempEdge tempEdge1 = new TempEdge();
+											//tempEdge1.setSource(String.valueOf(nodeId));
+											tempEdge1.setSource(typeStr);
+											tempEdge1.setType(relationshipAttr.getNamedItem("classification")
+													.getNodeValue());
+											tempEdge1.setTarget(relationshipAttr.getNamedItem("name").getNodeValue()); 
+											tempEdges.add(tempEdge1);
+//												buffer.append(relationshipAttr
+//														.getNamedItem("name")
+//														.getNodeValue());
+//												buffer.append('|');
+
+											nodeId=nodeId1;
+											nodeId1=0;
+										}
+										else {
+											TempEdge tempEdge2 = new TempEdge();
+											//tempEdge2.setSource(String.valueOf(nodeId));
+											tempEdge2.setSource(typeStr);
+											tempEdge2.setType(relationshipAttr.getNamedItem("classification")
+													.getNodeValue());
+											tempEdge2.setTarget(relationshipAttr.getNamedItem("name").getNodeValue()); 
+											tempEdges.add(tempEdge2);
+										}
+											
+										
 									}
 								}
-								if (buffer.length() > 0) {
-									out.write("reference=\"");
-									out.write(buffer.toString());
-									out.write("\" ");
-								}
-//								out.write("classification=\"null\" ");
-								out.write("/>");
-								out.write('\n');
-								nodeId++;
-
+//									if (buffer.length() > 0) {
+//										out.write("reference=\"");
+//										out.write(buffer.toString());
+//										out.write("\" ");
+//									}
+//									out.write("classification=\"null\" ");*/
+//									out.write("/>");
+//									out.write('\n');
+									nodeId++;
+								
 							} catch (IOException e1) {
 								// TODO Auto-generated catch block
 								e1.printStackTrace();
 							}
+						
+							
 						}
 					}
 				}
@@ -209,21 +311,27 @@ public class Odem2GraphML {
 	}
 
 	private void buildEdgeList() {
+		
 		for (int i = 0; i < tempEdges.size(); i++) {
 			TempEdge te = tempEdges.get(i);
+			String sourceID=null;
+			for(int k=0;k<nodes.size();k++){
+				if(nodes.get(k).equals(te.getSource()))
+					sourceID=String.valueOf(k);
+			}
 			for (int j = 0; j < nodes.size(); j++) {
 				if (te.getTarget().equals(nodes.get(j))) {
 					StringBuffer buffer = new StringBuffer();
 					buffer.append("<edge id=\"edge-");
 					buffer.append(i);
 					buffer.append("\" source=\"");
-					buffer.append(te.getSource());
+					buffer.append(sourceID);
 					buffer.append("\" target=\"");
 					buffer.append(j);
-					buffer.append("\" sourceId=\"");
-					buffer.append(te.getSource());
-					buffer.append("\" targetId=\"");
-					buffer.append(j);
+//					buffer.append("\" sourceId=\"");
+//					buffer.append(sourceID);
+//					buffer.append("\" targetId=\"");
+//					buffer.append(j);
 					buffer.append("\" type=\"");
 					buffer.append(te.getType());
 					buffer.append("\" isSelected=\"false\" state=\"null");
