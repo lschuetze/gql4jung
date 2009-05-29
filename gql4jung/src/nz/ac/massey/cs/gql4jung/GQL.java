e617ed24db22f49af2dfbdc0329cf86f041b0f70
jens.dietrich <jens.dietrich@gmail.com>
2009-05-29 03:42:29 +0000

    reorganised some packages updated license info in java files

--- gql4jung/src/nz/ac/massey/cs/gql4jung/GQL.java
+++ gql4jung/src/nz/ac/massey/cs/gql4jung/GQL.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2008 Jens Dietrich Licensed under the Apache License, Version 2.0 (the "License"); 
+ * Copyright 2009 Jens Dietrich Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
  * Unless required by applicable law or agreed to in writing, software distributed under the 
@@ -7,6 +7,7 @@
  * either express or implied. See the License for the specific language governing permissions 
  * and limitations under the License.
  */
+
 
 package nz.ac.massey.cs.gql4jung;
 
--- gql4jung/src/nz/ac/massey/cs/gql4jung/GroupByAggregation.java
+++ gql4jung/src/nz/ac/massey/cs/gql4jung/GroupByAggregation.java
@@ -1,13 +1,19 @@
-package nz.ac.massey.cs.gql4jung.util;
+/**
+ * Copyright 2009 Jens Dietrich Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
+ * Unless required by applicable law or agreed to in writing, software distributed under the 
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language governing permissions 
+ * and limitations under the License.
+ */
+
+
+package nz.ac.massey.cs.gql4jung;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
-import nz.ac.massey.cs.gql4jung.GroupByClause;
-import nz.ac.massey.cs.gql4jung.Motif;
-import nz.ac.massey.cs.gql4jung.MotifInstance;
-import nz.ac.massey.cs.gql4jung.Vertex;
 
 /**
  * Motif instance aggregation based on a collection of group by clauses 
