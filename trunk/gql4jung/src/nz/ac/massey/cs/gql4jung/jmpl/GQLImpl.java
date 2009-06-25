7086153ee139fc6a8d78104013ea62c78eccfea1
jens.dietrich@gmail.com <jens.dietrich@gmail.com@df725a96-ab7d-11dd-b9e7-8d5594d729c2>
2009-06-25 09:09:49 +0000

    introduced a new abstract GQL implementation to separate backtracking from the top level organisation of the derivation tree. This will facilitate concurrent implementations.

--- trunk/gql4jung/src/nz/ac/massey/cs/gql4jung/jmpl/GQLImpl.java
+++ trunk/gql4jung/src/nz/ac/massey/cs/gql4jung/jmpl/GQLImpl.java
@@ -12,10 +12,7 @@
 package nz.ac.massey.cs.gql4jung.jmpl;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import edu.uci.ics.jung.graph.*;
 import nz.ac.massey.cs.gql4jung.*;
 import nz.ac.massey.cs.gql4jung.util.PathCache;
@@ -24,33 +21,11 @@ import nz.ac.massey.cs.gql4jung.util.PathCache;
  * Improved graph query engine.
  * @author jens dietrich
  */
-public class GQLImpl extends Logging implements GQL {
+public class GQLImpl extends GQLImplCore {
 	public GQLImpl() {
 		super();
 	}
 
-	/*
-	private ObjectPool<List<Constraint>> agendaPool = new ObjectPool<List<Constraint>>(100) {
-
-		@Override
-		public List<Constraint> createNew() {
-			return new ArrayList<Constraint>();
-		}
-		@Override
-		public void reset(List<Constraint> l) {
-			l.clear();
-		}		
-	} ;
-	*/
-	
-	private boolean cancel = false;
-	private ConstraintScheduler scheduler = new SimpleScheduler();
-
-	@Override
-	public void cancel() {
-		cancel = true;
-		PathCache.switchCachingOff();
-	}
 
 	@Override
 	public void query(DirectedGraph<Vertex,Edge> graph, Motif motif, ResultListener listener,boolean ignoreVariants) {
@@ -88,126 +63,6 @@ public class GQLImpl extends Logging implements GQL {
 	    }
     	// reset caching
     	PathCache.switchCachingOff();
-	}
-
-	private void resolve(DirectedGraph<Vertex,Edge> graph, Motif motif, Controller controller, ResultListener listener) {
-		if (cancel) return;
-
-		// check for termination
-		if (controller.isDone()) {
-			MotifInstance instance = createInstance(graph,motif,controller);
-			listener.found(instance);
-			return;
-		}
-		
-		// back jumping
-		if (controller.isInJumpBackMode()) {
-			return;
-		}
-		
-		// recursion
-		Constraint nextConstraint = controller.next();  // one level down
-		
-		
-		if (LOG_GQL.isDebugEnabled()) {
-			LOG_GQL.debug("recursion level "+controller.getPosition()+", resolving: "+nextConstraint);
-		}
-		
-		if (nextConstraint instanceof PropertyConstraint) {
-			PropertyConstraint constraint = (PropertyConstraint)nextConstraint;
-			boolean result = false;
-			if (constraint.isSingleRole()) {
-				Vertex v = (Vertex)controller.lookup(constraint.getFirstRole());
-				if (v!=null) {
-					result = constraint.check(v);
-				}
-				else {
-					LOG_GQL.warn("encountered unresolved role "+constraint.getFirstRole()+", cannot resolve: "+constraint);
-				}
-			}
-			else {
-				Map<String,GraphElement> bind = new HashMap<String,GraphElement>();
-				for (Object role:constraint.getRoles()) {
-					Vertex v = (Vertex)controller.lookup(role.toString());
-					if (v!=null) {
-						bind.put(role.toString(),v);
-					}
-					else {
-						LOG_GQL.warn("encountered unresolved role "+constraint.getFirstRole()+", cannot resolve: "+constraint);
-					}	
-				}
-				result = constraint.check(bind); 
-			}
-			if (result) {
-				resolve(graph,motif,controller,listener);
-			}
-
-		}
-		else if (nextConstraint instanceof LoopInstruction) {
-			// full loop - this is the only way to progress
-			LoopInstruction in = (LoopInstruction)nextConstraint; 
-			String role = in.getRole();
-			for (Vertex v:(Collection<Vertex>)graph.getVertices()) {
-				controller.bind(role,v);
-				resolve(graph,motif,controller,listener);
-			}
-		}
-		else if (nextConstraint instanceof PathConstraint) {
-			PathConstraint constraint = (PathConstraint)nextConstraint; 
-			String sourceRole = constraint.getSource();
-			String targetRole = constraint.getTarget();
-			Vertex source = (Vertex)controller.lookup(sourceRole);
-			Vertex target = (Vertex)controller.lookup(targetRole);
-			if (source!=null && target!=null) {
-				Path result=constraint.check(graph, source, target); // path or edge
-				if (result!=null) {
-					controller.bind(constraint.getRole(),result);
-					resolve(graph,motif,controller,listener);
-				}
-			}
-			else if (source==null && target!=null) {
-				Iterator<Path> iter =constraint.getPossibleSources(graph,target);
-				resolveNextLevel(graph,motif,controller,listener,iter,target,sourceRole,constraint);
-				
-			}
-			else if (source!=null && target==null) {
-				Iterator<Path> iter =constraint.getPossibleTargets(graph,source);
-				resolveNextLevel(graph,motif,controller,listener,iter,source,targetRole,constraint);
-			}
-			else {
-				throw new IllegalStateException("cannot resolve linke constraints with two open slots");
-			}
-		}
-		controller.backtrack(); // one level up
-
-	}
-
-	private void resolveNextLevel(DirectedGraph<Vertex,Edge> graph, Motif motif, Controller controller, ResultListener listener, 
-			Iterator<Path> iter,Vertex end1,String end2Role,PathConstraint constraint) {
-		
-		while (iter.hasNext()) {
-			Path path = iter.next();
-			controller.bind(constraint.getRole(),path);
-			if (path.getStart()==end1) {
-				controller.bind(end2Role,path.getEnd());
-				resolve(graph,motif,controller,listener);
-			}
-			else if (path.getEnd()==end1) {
-				controller.bind(end2Role,path.getStart());
-				resolve(graph,motif,controller,listener);
-			}
-		}
-	}
-
-	private MotifInstance createInstance(Graph graph, Motif motif, Controller bindings) {
-		//System.out.println("creating result");
-		return new MotifInstanceImpl(motif,bindings);
-	}
-
-	@Override
-	public void reset() {
-
-
 	}
 
 }
