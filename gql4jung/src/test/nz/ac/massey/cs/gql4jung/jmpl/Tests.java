4150eea8dee9677d80aa30e29ef7770395584832
jens.dietrich <jens.dietrich@gmail.com>
2009-07-28 00:46:28 +0000

    moved result collector to util package

--- gql4jung/src/test/nz/ac/massey/cs/gql4jung/jmpl/Tests.java
+++ gql4jung/src/test/nz/ac/massey/cs/gql4jung/jmpl/Tests.java
@@ -25,6 +25,7 @@ import nz.ac.massey.cs.gql4jung.Vertex;
 import nz.ac.massey.cs.gql4jung.io.GraphMLReader;
 import nz.ac.massey.cs.gql4jung.jmpl.GQLImpl;
 import nz.ac.massey.cs.gql4jung.util.QueryResults;
+import nz.ac.massey.cs.gql4jung.util.ResultCollector;
 import nz.ac.massey.cs.gql4jung.xml.XMLMotifReader;
 import org.junit.After;
 import org.junit.Before;
